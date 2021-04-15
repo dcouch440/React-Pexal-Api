81c64bed1a9f635d5cfdee3b77583a8740196ea2
dcouch440 <dcouch440@gmail.com>
2021-04-15 12:24:23 -0700

    add build folder and depends

---
+++ build/precache-manifest.91590ee178940b0f266cf62314969926.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e857d7bc7a356a34041a62b5fb005ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5169f979af2f6344ae62",
+    "url": "/static/css/main.44a205ed.chunk.css"
+  },
+  {
+    "revision": "a9428b605857786134e4",
+    "url": "/static/js/2.4ea6bd61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4ea6bd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5169f979af2f6344ae62",
+    "url": "/static/js/main.f4c9d673.chunk.js"
+  },
+  {
+    "revision": "34ea8c9dee55631904b5",
+    "url": "/static/js/runtime-main.b30869ba.js"
+  },
+  {
+    "revision": "dcc07fb048cdccba933d8504dbad9db7",
+    "url": "/static/media/pinkOrangeSplash.dcc07fb0.jpg"
+  }
+]);
